fb973fe4cd2c572dfd3a2f6094a88a71d1e7de6c
Calli Herrmann <calliherrmann@gmail.com>
2023-03-01 18:52:34 -0500

    Add places to visit array

--- friends.js
+++ friends.js
@@ -1,3 +1,7 @@
 var arrays = ["Brandy", "Bailey", "Ian", "Bethany", "Anna"]
 
 console.log(arrays[0]);
+
+var placesToVisit = ["Rome", "Japan", "Costa Rica", "Ireland", "Greece"]
+
+console.log(placesToVisit.pop());
